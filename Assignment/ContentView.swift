487575e4606fc2dacdd2695d5dbdb5e2f3d2b0cd
aannuuj <hariompalkar@gmail.com>
2020-11-18 22:03:22 +0530

    Initial Commit

---
+++ Assignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment
+//
+//  Created by Hariom Palkar on 18/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
